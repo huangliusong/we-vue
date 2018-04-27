999352c5739f911fc4b089424ad52fbedda76742
tianyong90 <412039588@qq.com>
2018-04-28 01:46:16 +0800

    improv typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,50 @@
 import Vue from 'vue'
+import { WvComponent } from './component'
+
+// TODO
+
+export const version: string
+export function install(vau: typeof Vue): void
+export class Actionsheet extends WvComponent {}
+export class Badge extends WvComponent {}
+export class Button extends WvComponent {}
+export class Cell extends WvComponent {}
+export class CellSwipe extends WvComponent {}
+export class CellSwipeButton extends WvComponent {}
+export class Checklist extends WvComponent {}
+export class Circle extends WvComponent {}
+export class DatetimePicker extends WvComponent {}
+export class Dialog extends WvComponent {}
+export class Flex extends WvComponent {}
+export class FlexItem extends WvComponent {}
+export class Footer extends WvComponent {}
+export class FormPreview extends WvComponent {}
+export class Grid extends WvComponent {}
+export class GridItem extends WvComponent {}
+export class Group extends WvComponent {}
+export class Header extends WvComponent {}
+export class Icon extends WvComponent {}
+export class InfiniteScroll extends WvComponent {}
+export class Input extends WvComponent {}
+export class Lazyload extends WvComponent {}
+export class Loadmore extends WvComponent {}
+export class MediaBox extends WvComponent {}
+export class Navbar extends WvComponent {}
+export class NavbarItem extends WvComponent {}
+export class NumberSpinner extends WvComponent {}
+export class Panel extends WvComponent {}
+export class Picker extends WvComponent {}
+export class Popup extends WvComponent {}
+export class Progress extends WvComponent {}
+export class Radio extends WvComponent {}
+export class SearchBar extends WvComponent {}
+export class Slider extends WvComponent {}
+export class Spinner extends WvComponent {}
+export class Swipe extends WvComponent {}
+export class SwipeItem extends WvComponent {}
+export class Switch extends WvComponent {}
+export class Tabbar extends WvComponent {}
+export class TabbarItem extends WvComponent {}
+export class Textarea extends WvComponent {}
+export class Toast extends WvComponent {}
+export class TopTips extends WvComponent {}
